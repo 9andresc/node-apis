5c3ab7e4672e48788b8f7333c7a10027ceb57131
Andres Cabral <andrescabral.c@gmail.com>
2020-11-10 16:05:25 -0300

    chore: add initial project config

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  rules: {
+    'prettier/prettier': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-extra-semi': 'off'
+  }
+}
